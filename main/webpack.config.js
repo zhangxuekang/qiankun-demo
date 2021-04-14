46731c26e5b236e5e0b3f4d92cd911ba937600e4
zhangxk <zhangxuekang@yuanfudao.com>
2021-04-14 14:21:19 +0800

    init

---
+++ main/webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+  devServer: {
+    hot: true,
+    historyApiFallback: true,
+  },
+};
